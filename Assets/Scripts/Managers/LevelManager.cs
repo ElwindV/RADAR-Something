4186fdab9d5635ef9c96374ad739b8a79fed2329
Elwin de Vries <elwin.de.vries@hva.nl>
2021-10-30 19:11:05 +0200

    Added script

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public void Awake()
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        }
+
+        public void FixedUpdate()
+        {
+            if (Input.GetKey("escape"))
+            {
+                QuitGame();
+            }
+        }
+
+        private static void QuitGame()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+            Application.Quit();
+        }
+    }
+}
